b5b8eb95119bc146258e73eef67a73916249dc5e
titantooth <ali.rabieeie@gmail.com>
2023-08-12 10:05:46 +0330

    finished business section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <div>
+      <button
+        type="button"
+        className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+      >
+        Get Started
+      </button>
+    </div>
+  );
 };
 
 export default Button;
